7c427f83c1f30f3f209c53aee013bf2c4373c9a9
talshnitzer <43995087+talshnitzer@users.noreply.github.com>
2021-02-28 09:16:06 +0200

    upon tring to enter add recomendation page, redirect to login page if user not logged in

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -23,7 +23,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
             <Component {...props} />
           </div>
         ) : (
-          <Redirect to="/" />
+          <Redirect to="/login" />
         )
       }
     />
